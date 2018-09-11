e2176ab8b07ab4c6a9982c40600706223cc752a7
Memia <memia@hotmail.com.au>
2018-09-11 15:40:03 +1000

    Lesson01
    We used the plug in "navmesh components" to make a general navmesh + offlink to make agent navigate on 6 faces of a cube.

---
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+//This script handles player movement and rotation logic
+public class PlayerController : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    private Vector3 targetPos;
+    // Use this for initialization
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (targetPos.magnitude >0)
+        {
+            agent.SetDestination(targetPos);
+        }
+    }
+    public void SetTarget(Vector3 target)
+    {
+        targetPos = target;
+    }
+}
---
+++ New Unity Project/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script is in charge of receiving input from the user and interacting with PlayerController
+public class PlayerInput : MonoBehaviour
+{
+
+    public PlayerController controller;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(camRay, out hit, 1000f))
+            {
+                controller.SetTarget(hit.point);
+            }
+        }
+    }
+}
